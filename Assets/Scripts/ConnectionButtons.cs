2d4457bbefb0ae4f3986948301e44dd3ecfd55ec
Michael Bychkowski <mbychkowski@google.com>
2023-07-03 12:15:17 -0400

    First commit

---
+++ Assets/Scripts/ConnectionButtons.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class ConnectionButtons : MonoBehaviour
+{
+  public void StartClient()
+  {
+    NetworkManager.Singleton.StartClient();
+  }
+  public void StartHost()
+  {
+    NetworkManager.Singleton.StartHost();
+  }
+}
